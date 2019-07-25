81b3819f2af8aca9d14f0239c4d667ba41bd9eba
me1234q <me1234q@gmail.com>
2019-07-24 18:08:05 -0700

    Ful Funcitonality

--- src/main/java/frc/team972/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/team972/robot/subsystems/DriveSubsystem.java
@@ -14,8 +14,11 @@ public class DriveSubsystem extends Subsystem {
     DoubleSolenoid s = new DoubleSolenoid(6, 7);
     Joystick j = new Joystick(0);
 
-    WPI_TalonSRX t1 = new WPI_TalonSRX(3);
-    WPI_TalonSRX t2 = new WPI_TalonSRX(4);
+    WPI_TalonSRX t1 = new WPI_TalonSRX(2);
+    WPI_TalonSRX t2 = new WPI_TalonSRX(1);
+
+    WPI_TalonSRX a1 = new WPI_TalonSRX(3);
+    WPI_TalonSRX a2 = new WPI_TalonSRX(4);
 
     public DriveSubsystem(Robot robot) {
         super(robot);
@@ -37,16 +40,32 @@ public class DriveSubsystem extends Subsystem {
     }
 
     public void teleopPeriodic() {
-        if (j.getRawButton(1) && !j.getRawButton(2)) {
+        if (j.getRawButton(5) && !j.getRawButton(6)) {
             s.set(DoubleSolenoid.Value.kForward); 
-        } else if (j.getRawButton(2) && !j.getRawButton(1)) {
+        } else if (j.getRawButton(6) && !j.getRawButton(5)) {
             s.set(DoubleSolenoid.Value.kReverse); 
         } else {
             s.set(DoubleSolenoid.Value.kOff); 
         }
 
-        t1.set(ControlMode.PercentOutput, j.getRawAxis(1)/2);
+        t1.set(ControlMode.PercentOutput, -j.getRawAxis(1)/2);
         t2.set(ControlMode.PercentOutput, j.getRawAxis(5)/2);
+
+        if (j.getRawButton(1) && !j.getRawButton(2)) {
+            a1.set(ControlMode.PercentOutput, 0.5);
+        } else if (j.getRawButton(2) && !j.getRawButton(1)) {
+            a1.set(ControlMode.PercentOutput, -0.5);
+        } else {
+            a1.set(ControlMode.PercentOutput, 0);
+        }
+
+        if (j.getRawButton(3) && !j.getRawButton(4)) {
+            a2.set(ControlMode.PercentOutput, 0.5);
+        } else if (j.getRawButton(4) && !j.getRawButton(3)) {
+            a2.set(ControlMode.PercentOutput, -0.5);
+        } else {
+            a2.set(ControlMode.PercentOutput, 0);
+        }
     }
 
     public void outputTelemetry() {
